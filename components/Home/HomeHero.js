f6e66dc54b7f5134ebf67db51ff134bddec84b53
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-04-07 13:34:13 +1000

    Create HomeHero component.

--- components/Home/HomeHero.js
+++ components/Home/HomeHero.js
@@ -1,10 +1,31 @@
+import Image from "next/image";
+import Rocket from "@/assets/rocket.png";
 import styles from "./HomeHero.module.css";
 
 const HomeHero = () => {
   return (
-    <section className={styles.heroContainer}>
-      <h1>Hello Next</h1>
-    </section>
+    <div className='section'>
+      <div className={styles.heroContainer}>
+        <div className={styles.textContainer}>
+          <h1>Cryptocurrency Exchange </h1>
+          <div className='underline'></div>
+          <h3>Journey Starts HERE</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Augue
+            adipiscing porta aliquet eget pellentesque suspendisse. Tortor donec
+            vivamus vestibulum donec tristique vestibulum faucibus scelerisque
+            lobortis. Elementum, egestas fermentum sit egestas. Pulvinar mollis
+            sed aliquet in vulputate ac risus nisi, id. Eros, at vitae id cursus
+            amet scelerisque sit. Enim lacinia in tellus tellus in elit aenean
+            volutpat. Ac cursus enim risus nec commodo sollicitudin.
+          </p>
+          <button className='primaryBtn'>Get Started</button>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image width={550} src={Rocket} alt='Rocket image' />
+        </div>
+      </div>
+    </div>
   );
 };
 
