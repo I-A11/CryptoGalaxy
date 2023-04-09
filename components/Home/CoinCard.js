d12713ec2f6914cf4294e85597ce1952a58c9cac
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-04-09 14:53:34 +1000

    Style coin card component.

--- components/Home/CoinCard.js
+++ components/Home/CoinCard.js
@@ -1,5 +1,10 @@
-import styles from "./CoinCard.module.css";
 import Image from "next/image";
+import styles from "./CoinCard.module.css";
+
+const priceFormat = new Intl.NumberFormat("en-us", {
+  style: "currency",
+  currency: "AUD",
+});
 
 const CoinCard = ({
   id,
@@ -12,19 +17,29 @@ const CoinCard = ({
   return (
     <div className={styles.cardContainer}>
       <div className={styles.cardHeader}>
-        <p>{symbol}</p>
-        <p> {id}</p>
+        <Image src={image} width={35} height={35} alt={name} />
+        <div className={styles.coinName}>
+          <div> {name}</div>
+          <div className={styles.coinSymbol}>{symbol}</div>
+        </div>
       </div>
+      <div className={styles.underline}></div>
       <div className={styles.cardBody}>
-        {/* <Image src={image} width={20} height={20} alt={name} /> */}
-        <p>{name}</p>
-      </div>
-      <div className={styles.cardBody}>
-        <p>{current_price}</p>
-        <p>{price_change_percentage_24h}</p>
+        <div className={styles.price}>{priceFormat.format(current_price)}</div>
+        <div className={styles.change}>
+          {price_change_percentage_24h < 0 ? (
+            <p className={styles.coinPercentRed}>
+              ({price_change_percentage_24h.toFixed(2)}%)
+            </p>
+          ) : (
+            <p className={styles.coinPercentGreen}>
+              ({price_change_percentage_24h.toFixed(2)}%)
+            </p>
+          )}
+        </div>
       </div>
       <div className={styles.cardChart}>
-        <p>chart</p>
+        <di>chart</di>
       </div>
     </div>
   );
