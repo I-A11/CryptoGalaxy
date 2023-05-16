804558692e2656c647c2b578ad12b9989cb34585
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-05-16 12:07:17 +1000

    Start working in explore page to display coins table.

--- pages/explore.js
+++ pages/explore.js
@@ -1,11 +1,30 @@
-import React from "react";
+import useAxios from "@/hooks/useAxios";
+import styles from "./explore.module.css";
+import Coin from "@/components/Home/Explore/Coin";
 
-const explore = () => {
+const Explore = () => {
+  const { response, loading } = useAxios(
+    "coins/markets?vs_currency=aud&order=market_cap_desc&per_page=20&page=1&sparkline=false&locale=en"
+  );
+
   return (
-    <div className='headerBottom'>
-      <h1>Explore</h1>
+    <div className="section">
+      <div className="headerBottom">
+        <div className={styles.marketContainer}>
+          <h1>
+            Explore <span className="text-gradient">Markets</span>
+          </h1>
+          <div className="underline underline-center"></div>
+          <div className={styles.coinsContainer}>
+            {response &&
+              response.map((coin) => {
+                return <Coin coin={coin} key={coin.id} />;
+              })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default explore;
+export default Explore;
